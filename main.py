2a31d3471b50c5a8a459826e8db62a74b8aca35f
Patricio Costilla <patriciocostilla@gmail.com>
2020-11-03 12:22:40 +0000

    getting only content from search

--- main.py
+++ main.py
@@ -11,23 +11,39 @@ def google_search(search_term, **kwargs):
     return res
 
 def get_page_text(link):
-    r = requests.get(link)
-    soup = BeautifulSoup(r.text, features="html.parser")
-    page_text = soup.body.get_text()
-    return page_text.strip()
+    try:
+        r = requests.get(link)
+        soup = BeautifulSoup(r.text, features="html.parser")
+        page_text = soup.body.get_text()
+        return page_text.strip()
+    except:
+        return "Scraping error"
 
 def get_content_from_search(search_term):
     r = google_search(search_term)
-    results = r["items"][:MAX_AMOUNT]
-    for result in results:
+    search_results = r["items"]
+    scraped_content = []
+    for result in search_results:
         page_text = get_page_text(result['link'])
-        return (result['title'], result['link'], page_text)
+        scraped_content.append({'title': result['title'], 'link': result['link'], 'page_text': page_text})
+        if len(scraped_content) >= MAX_AMOUNT:
+            break
+    return scraped_content
+
+def get_content_only(search_term):
+    scraped_content = get_content_from_search(search_term)
+    content = ""
+    for item in scraped_content:
+        content = content + "\n" + item['page_text']
+    return content
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Wrong amount of parameters passed, usage: main.py <search-term>")
     else:
-        contentTuple = get_content_from_search(sys.argv[1])
-        print(contentTuple)
+        # scraped_content = get_content_from_search(sys.argv[1])
+        # print(scraped_content)
+        content = get_content_only(sys.argv[1])
+        print(content)
     
