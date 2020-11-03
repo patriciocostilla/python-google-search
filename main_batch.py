327c3b0ec681a6df8fd37c9d850d84418874fd2b
Patricio Costilla <patriciocostilla@gmail.com>
2020-11-03 12:22:19 +0000

    fixing batch search

---
+++ main_batch.py
@@ -0,0 +1,9 @@
+from main import *
+import time
+
+if __name__ == "__main__":
+    for i in range(10000):
+        time.sleep(0.5)
+        print(i)
+        get_content_from_search(f"Search {i}")
+
